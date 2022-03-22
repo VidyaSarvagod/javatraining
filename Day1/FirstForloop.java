87f963c0762f409903c90c6e50cdc718bdcabf28
VidyaSarvagod <vidyahire01@gmail.com>
2022-03-22 04:46:08 -0700

    Basic For Loop using Jshell

---
+++ Day1/FirstForloop.java
@@ -0,0 +1,13 @@
+public class FirstForloop {
+    public static void main(String arg[]){
+        int i=1;
+        do{
+            System.out.printf("%d",i*2).println();
+
+            i++;
+
+
+        }while(i<=10);
+
+    }
+}
