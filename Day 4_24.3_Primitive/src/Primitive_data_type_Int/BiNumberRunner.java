af93219c0a88b24cce3d0e9c4e76d4aa0d5d75bb
VidyaSarvagod <vidyahire01@gmail.com>
2022-03-24 05:00:10 -0700

    Primitive Data Type

---
+++ Day 4_24.3_Primitive/src/Primitive_data_type_Int/BiNumberRunner.java
@@ -0,0 +1,14 @@
+package Primitive_data_type_Int;
+
+public class BiNumberRunner {
+    public static void main(String[] args) {
+        BiNumber number=new BiNumber(2,3);
+         System.out.println(number.add());
+         System.out.println(number.multiplication());
+         number.doubleofno();
+        System.out.println(number.getNumber1());
+        System.out.println(number.getNumber2());
+
+    }
+
+}
